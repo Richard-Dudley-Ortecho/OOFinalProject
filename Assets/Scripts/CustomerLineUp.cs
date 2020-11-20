ad84289c2b9592e1a95320b11ebe01b356e6e7c6
Richard-Dudley-Ortecho <dudley.o@hotmail.com>
2020-11-20 14:51:18 -0800

    Not working, but Patterns are there

---
+++ Assets/Scripts/CustomerLineUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerLineUp : IObserver
+{
+    public CustomerBehavior lineUpCustomer1, lineUpCustomer2, lineUpCustomer3;
+
+
+
+    public override void OnNotify(int timePassed)
+    {
+        lineUpCustomer1.updateTimePassed(timePassed);
+        lineUpCustomer2.updateTimePassed(timePassed);
+        lineUpCustomer3.updateTimePassed(timePassed);
+        if(timePassed == 5) {
+            activateScedule(lineUpCustomer1);
+        }
+        if (timePassed == 25) {
+            activateScedule(lineUpCustomer2);
+        }
+
+        if (timePassed == 45) {
+            activateScedule(lineUpCustomer3);
+        }
+    }
+
+    public void activateScedule(CustomerBehavior c)
+    {
+        c.activateCustomer();
+    }
+}
---
+++ Assets/Scripts/IObservable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IObservable : MonoBehaviour
+{
+    List<IObserver> observers = new List<IObserver>();
+
+    public void Notify(int typeOfNotification)
+    {
+        for (int i=0; i<observers.Count; i++)
+        {
+            observers[i].OnNotify(typeOfNotification);
+        }
+    }
+
+    public void AddObserver(IObserver observer)
+    {
+        observers.Add(observer);
+    }
+
+    public void RemoveObserver(IObserver observer)
+    {
+        //TODO
+    }
+}
+
+public abstract class IObserver : MonoBehaviour
+{
+    public abstract void OnNotify(int notifcationType);
+}
